af9681b5ccc569bce2e0d8e2b44f067050ba66cf
Rose <youngconfucian@outlook.com>
2024-11-12 16:35:22 +0800

    update results

--- experiments/stats_uccsd_su4.py
+++ experiments/stats_uccsd_su4.py
@@ -27,9 +27,9 @@ def su4_circ_stats(qasm_fname):
     """Statistic of #2Q and Depth-2Q of SU(4)-based circuit."""
     circ = bqskit.Circuit.from_file(qasm_fname)
     blocks = list(bqskit_compiler.compile(circ, workflow))
-    fused_2q = Circuit([gates.UnivGate(blk.get_unitary().numpy).on(list(blk.location)) for blk in blocks])
-    circ_su4 = Circuit([g for g in fused_2q if not is_tensor_prod(g.data)])
-    return circ_su4.num_gates, len(circ_su4.layer())
+    circ_su4 = Circuit(
+        [gates.X.on(blk.location[0], blk.location[1]) for blk in blocks if not is_tensor_prod(blk.get_unitary().numpy)])
+    return circ_su4.num_gates, circ_su4.depth
 
 
 selected_benchmarks = [
@@ -43,8 +43,7 @@ selected_benchmarks = [
     'H2O_frz_JW'
 ]
 
-BENCHMARK_DPATH = '../benchmarks/uccsd_qasm'
-OUTPUT_DPATH = './output_hamlib/'
+OUTPUT_DPATH = './output_uccsd/'
 
 result = pd.DataFrame(columns=['program', 'num_su4(tket)', 'depth_su4(tket)',
                                'num_su4(paulihedral)', 'depth_su4(paulihedral)',
@@ -55,15 +54,18 @@ result['program'] = selected_benchmarks
 for compiler in ['tket', 'paulihedral', 'tetris', 'phoenix']:
     num_su4_list = []
     depth_su4_list = []
-    output_dpath = os.path.join(OUTPUT_DPATH, compiler)
-    for program_name in selected_benchmarks:
-        fname = os.path.join(BENCHMARK_DPATH, program_name + '.qasm')
-        num_su4, depth_su4 = su4_circ_stats(os.path.join(output_dpath, fname))
+    output_dpath = os.path.join(OUTPUT_DPATH, compiler, 'all2all_opt')
+    print(f'Processing {output_dpath}...')
+    for program_name in tqdm(selected_benchmarks):
+        fname = os.path.join(output_dpath, program_name + '_sto3g' + '.qasm')
+        num_su4, depth_su4 = su4_circ_stats(fname)
         num_su4_list.append(num_su4)
         depth_su4_list.append(depth_su4)
 
     result[f'num_su4({compiler})'] = num_su4_list
     result[f'depth_su4({compiler})'] = depth_su4_list
 
-result.to_csv('su4_stats.csv', index=False)
+result.to_csv('./results/result_uccsd_su4_stats.csv', index=False)
 print(result)
+
+bqskit_compiler.close()
