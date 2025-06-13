52f5239bc3e3c5f14a090b7b1d441aaa3d6b0c1e
Anan <youngcius007@gmail.com>
2025-06-14 03:17:30 +0800

    fix typos

--- phoenix/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Phoenix is a Python package for compiling VQA programs whose high-level IR is Pauli strings with coefficients.
-"""
-from .basic import gates, circuits
-from .basic.gates import Gate
-from .basic.circuits import Circuit, QASMStringIO
-from . import synthesis
-from . import models
-from . import utils
--- phoenix/synthesis/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import grouping
-from . import ordering
-from . import simplification
-from . import utils
